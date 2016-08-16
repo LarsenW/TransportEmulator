4d705082c3c2da222f5204ad780d9367b67b05f4
LarsenW <wolflarsen10@gmail.com>
2016-08-16 21:43:23 +0300

    refactor errors

--- common/errors/deactivation-error.js
+++ common/errors/deactivation-error.js
@@ -2,12 +2,13 @@
  * Created by dmytro on 15.08.16.
  */
 'use strict';
-module.exports = function DeactivationError(message) {
+function DeactivationError(message) {
     Error.call(this, message);
     this.name = 'EmptyQueueError';
     this.message = message;
 
     Error.captureStackTrace(this, DeactivationError);
-
-    DeactivationError.prototype = Object.create(Error.prototype);
 }
+DeactivationError.prototype = Object.create(Error.prototype);
+
+module.exports=DeactivationError;
