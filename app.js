d35100728c2a451cf797cb0c1e36211b16a8155f
LarsenW <wolflarsen10@gmail.com>
2016-08-19 14:03:04 +0300

    code cleaning

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 let cron = require('node-cron');
 let config = require('./config');
 let MongoClient = require('mongodb').MongoClient;
-let assert = require('assert');
 let carService = require('./services/car-service');
 let orderService = require('./services/order-service');
 let deliveryNotifier = require('./services/delivery-notifier');
@@ -14,7 +13,6 @@ cron.schedule('* * * * *', function () {
     console.log('running a task every minute');
     MongoClient.connect(url, function (err, db) {
             if (!err) {
-                assert.equal(null, err);
                 console.log("Connected succesfully to DB server");
                 carService.handleArrivedCars(db, function (ordersArray) {
                     return carService.checkActiveCars(db)
